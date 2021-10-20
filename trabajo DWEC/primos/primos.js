dda30b05bed3990ab6bb25b1d3ebbbc3bd332256
AntonioJ93 <86188263+AntonioJ93@users.noreply.github.com>
2021-10-20 17:05:09 +0200

    +

--- trabajo DWEC/primos/primos.js
+++ trabajo DWEC/primos/primos.js
@@ -1,27 +1,30 @@
 // obtener 100 primeros números primos
-const max=100;
+const max = 100;
 
-function esPrimo(numero){
-    if(numero===1){
+function esPrimo(numero) {
+    if (numero === 1) {
         return true;
     }
-    let primo=false;
-    for (let index = numero; index > 2 ; index--) {
-        if((numero % (index-1))===0){
-          return false;
-        }else{
-            primo=true;
+    let primo = false;
+
+    for (let index = numero; index > 2; index--) {
+        // evito dividir por si mismo y por uno para comprobar el resto
+        // si se cumple no es primo
+        if ((numero % (index - 1)) === 0) {
+            return false;
+        } else {
+            primo = true;
         }
     }
     return primo;
 }
 
-function obtenerNPrimos(nPrimos){
-    let primos=[] ;
+function obtenerNPrimos(nPrimos) {
+    let primos = [];
     for (let index = 0; primos.length < nPrimos; index++) {
-            if (esPrimo(index)) {
-                primos.push(index);
-         }
+        if (esPrimo(index)) {// si es primo lo añado al array
+            primos.push(index);
+        }
     }
     return primos;
 }
