14cc5b6ea1a597c09ea1b0a2fca85704393dbda0
AntonioJ93 <86188263+AntonioJ93@users.noreply.github.com>
2022-02-16 15:42:40 +0100

    +

--- tarea4/participante/participante.js
+++ tarea4/participante/participante.js
@@ -1,61 +1,164 @@
 "use strict"
 
-/* input apellidos y nombre en mayusculas*/
-nombre.addEventListener("blur",function (e) {
+
+nombre.addEventListener("blur", function (e) {
     pasarAMayusculas(e.target);
-},false);
+    relleno(e.target);
+}, false);
 
-apellidos.addEventListener("blur",function (e) {
+apellidos.addEventListener("blur", function (e) {
     pasarAMayusculas(e.target);
-},false);
+    relleno(e.target);
+}, false);
 
-function pasarAMayusculas(elemento) {
-    let txt=elemento.value;
-    elemento.value=txt.toUpperCase();
-}
+anno.addEventListener("input", function (e) {
+    validarAnno();
+}, false);
 
-let regexAno=/^(19[5-9][7-9]|200[0-3])$/;
-anno.addEventListener("input",function(e){
-    validarAnno(e.target);
-},false);
-
-usuario.addEventListener("input",function(e){
-    validarNombreUsuario(e.target);
-},false);
-
-/* año de nacimiento entre 1957 y 2003 */
-const MIN_ANNO=1957;
-const MAX_ANNO=2003;
+usuario.addEventListener("input", function (e) {
+    validarNombreUsuario();
+}, false);
 
 
+pass.addEventListener("input", function (e) {
+    validarPass();
+}, false);
 
-function validarAnno(elemento){
-    let error=elemento.parentNode.querySelector(".error");
-        if(regexAno.test(elemento.value)){
-            //rango correcto
-            error.innerHTML="";
+passConfirm.addEventListener("input", function (e) {
+    validarPassConfirm();
+}, false);
+
+
+/*
+    SUBMIT
+*/
+formulario.addEventListener("submit", function (e) {
+
+    if (relleno(nombre) && relleno(apellidos)
+        && validarAnno() && validarNombreUsuario()
+        && validarPass() && validarPassConfirm()
+        && validarJornada()) {
+        let labelsJornadas = [];
+        let jornadas = formulario.querySelectorAll("input[type=checkbox]:checked")
+        jornadas.forEach((jornada) => {
+            labelsJornadas.push(jornada.nextSibling.nextSibling.textContent);
+
+        });
+        console.log(labelsJornadas);
+        if (!confirm(`Desea enviar el formulario con:\nNombre: ${nombre.value}\nApellidos: ${apellidos.value}\nAño: ${anno.value}\nContraseña: ${pass.value}\nConfirmar Contraseña: ${passConfirm.value}\nJornadas:${labelsJornadas}`)) {
+            //cancelas
+            e.preventDefault();
             return;
         }
-    error.innerHTML="El año debe estar comprendido entre "+MIN_ANNO+" y "+MAX_ANNO;
+        //aceptas y se envia
+        return;
+    }
+    //datos no válidos
+    e.preventDefault();
+}, false);
+
+/*
+    RESET
+*/
+formulario.addEventListener("reset", function () {
+    resetError();
+}, false);
+
+
+/* input apellidos y nombre en mayusculas*/
+function pasarAMayusculas(elemento) {
+    let txt = elemento.value;
+    elemento.value = txt.toUpperCase();
 }
 
+/* campo relleno */
+function relleno(elemento) {
+    let error = elemento.parentNode.querySelector(".error");
+    if (elemento.value.trim() != "") {
+        error.innerHTML = "";
+        return true;
+    }
+    error.innerHTML = "El campo no puede estar vacio";
+    return false;
+}
+
+/* año de nacimiento entre 1957 y 2003 */
+function validarAnno() {
+    const MIN_ANNO = 1957;
+    const MAX_ANNO = 2003;
+    let regexAno = /^(19[5-9][7-9]|200[0-3])$/;
+    let error = anno.parentNode.querySelector(".error");
+    if (regexAno.test(anno.value)) {
+        //rango correcto
+        error.innerHTML = "";
+        return true;
+    }
+    error.innerHTML = "El año debe estar comprendido entre " + MIN_ANNO + " y " + MAX_ANNO;
+    return false;
+}
 
 /*
     VALIDAR USUAIRO
 */
 
-
-const MIN_USER_LENGTH=5;
-const MAX_USER_LENGTH=15;
-let regexUsuario=/^[B-DF-HJ-NP-TV-Z][\wäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙñÑ]{4,15}$/;
-
-
-function validarNombreUsuario(elemento){
-    let error=elemento.parentNode.querySelector(".error");
-    console.log(elemento.value);
-    if(regexUsuario.test(elemento.value)){
-        error.innerHTML="";
-        return;
+function validarNombreUsuario() {
+    const MIN_USER_LENGTH = 5;
+    const MAX_USER_LENGTH = 15;
+    let regexUsuario = /^[B-DF-HJ-NP-TV-Z][\wäÄëËïÏöÖüÜáéíóúáéíóúÁÉÍÓÚÂÊÎÔÛâêîôûàèìòùÀÈÌÒÙñÑ]{4,14}$/;
+    let error = usuario.parentNode.querySelector(".error");
+    console.log(usuario.value);
+    if (regexUsuario.test(usuario.value)) {
+        error.innerHTML = "";
+        return true;
     }
-    error.innerHTML=`Sólo caracteres alfanúmericos, "_" y estar entre ${MIN_USER_LENGTH} y ${MAX_USER_LENGTH}`;
+    error.innerHTML = `Sólo caracteres alfanúmericos, "_" y estar entre ${MIN_USER_LENGTH} y ${MAX_USER_LENGTH}`;
+    return false
+}
+
+/*
+    VALIDAR PASSSWORD
+*/
+
+function validarPass() {
+    //cualquier caracter par, signos una letra matuscula palabra pss excluida
+    let patronPass = /^(?=.*[02468])(?=.*[$&@#])(?=.*[A-Z])(?!.*pass.*)\S{8,20}$/;
+    let error = pass.parentNode.querySelector(".error");
+    if (patronPass.test(pass.value)) {
+        //pass ok
+        error.innerHTML = "";
+        return true;
+    }
+    //pass mala
+    error.innerHTML = `La contraseña no es válida`;
+    return false;
+
+
+}
+
+function validarPassConfirm() {
+    let error = passConfirm.parentNode.querySelector(".error");
+    if (passConfirm.value == pass.value) {
+        //pass ok
+        error.innerHTML = "";
+        return true;
+    }
+    //pass mala
+    error.innerHTML = `La contraseña no coincide`;
+    return false;
+
+}
+
+function validarJornada() {
+    let check = formulario.querySelectorAll("input[type=checkbox]:checked");
+    if (check.length > 0 && check.length <= 3) {
+        jornadaError.innerHTML = "";
+        return true;
+    }
+    jornadaError.innerHTML = "Debes seleccionar entre 1 y 3 jornadas"
+    return false;
+}
+
+function resetError() {
+    let errores = formulario.querySelectorAll(".error");
+    errores.forEach(error => error.innerHTML = "");
 }
