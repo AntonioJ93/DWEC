58af4e5383a41dc5ab4941995615e11b3f58d2d0
AntonioJ93 <86188263+AntonioJ93@users.noreply.github.com>
2022-02-16 21:52:31 +0100

    pattern html falla

--- tarea4/ponencias/ponencias.js
+++ tarea4/ponencias/ponencias.js
@@ -8,6 +8,7 @@ formulario.addEventListener("submit",function(e){
     document.getElementById("tabla-titulo").innerHTML=titulo.value;
     document.getElementById("tabla-descripcion").innerHTML=descripcion.value;
     document.getElementById("tabla-ponente").innerHTML=ponente.value;
+    document.getElementById("tabla-categoria").innerHTML=categoria.value;
     document.getElementById("tabla-aforo").innerHTML=aforo.value;
     tabla.style.display="block";
     e.preventDefault();
